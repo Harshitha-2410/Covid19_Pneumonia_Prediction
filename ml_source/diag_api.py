a1e128e743aa27784c54da98ae53abc23543679c
Harshitha-2410 <harshithadasabohina@gmail.com>
2025-07-30 12:07:22 +0530

    Add files via upload

---
+++ ml_source/diag_api.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+
+model=joblib.load("D:\\Covid19\\ml_source\\covid_diag.pkl")
+
+class inp(BaseModel):
+    Age:int
+    Gender:int
+    Fever:int
+    Cough:int
+    Fatigue:int
+    Breathlessness:int
+    Comorbidity:int
+    Stage:int
+    Type:int
+    Tumor_Size:float
+
+app=FastAPI()
+
+@app.get("/")
+def route():
+    return {"message":"Welcome"}
+
+@app.post("/prd")
+def prediction(data:inp):
+    inp=pd.DataFrame([data.dict()])
+    pred=model.predict(inp)
+    return {"Prediction":pred[0]}
